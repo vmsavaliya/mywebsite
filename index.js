9e551e99eb406de065e9ca7145f2b2ec7a7fbf23
vmsavaliya <103499143+vmsavaliya@users.noreply.github.com>
2022-08-04 09:12:25 +0530

    MyWebsite

---
+++ index.js
@@ -0,0 +1,169 @@
+/*  ======================================
+    creating a responsive navbar tabbed component
+    ======================================*/
+
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const headerElem = document.querySelector(".header");
+
+mobile_nav.addEventListener("click", () => {
+  headerElem.classList.toggle("active");
+});
+
+// ========================================
+// sticky navigation
+// ========================================
+const sectionHero = document.querySelector(".header");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+    console.log(ent);
+    !ent.isIntersecting
+      ? document.body.classList.add("sticky")
+      : document.body.classList.remove("sticky");
+  },
+  {
+    // viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-100px",
+  }
+);
+
+// when the hero section end part reached then we need to show the sticky navigation
+observer.observe(sectionHero);
+
+/*  ======================================
+    creating a portfolio tabbed component
+    ======================================*/
+
+const p_btns = document.querySelector(".p-btns");
+const p_btn = document.querySelectorAll(".p-btn");
+const p_img_elem = document.querySelectorAll(".img-overlay");
+
+p_btns.addEventListener("click", (e) => {
+  const p_btn_clicked = e.target;
+  console.log(p_btn_clicked);
+
+  if (!p_btn_clicked.classList.contains("p-btn")) return 
+
+  p_btn.forEach((curElem) => curElem.classList.remove("p-btn-active"));
+
+  p_btn_clicked.classList.add("p-btn-active");
+
+  // to find the number is data attr
+  const btn_num = p_btn_clicked.dataset.btnNum;
+  console.log(btn_num);
+
+  const img_active = document.querySelectorAll(`.p-btn--${btn_num}`);
+
+  p_img_elem.forEach((curElem) => curElem.classList.add("p-image-not-active"));
+
+  img_active.forEach((curElem) =>
+    curElem.classList.remove("p-image-not-active")
+  );
+  // p_img_elem
+  // p-btn--1
+});
+
+
+
+
+const myJsmedia = (widthSize) => {
+  if (widthSize.matches) {
+    /*var swiper =*/ new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    });
+  } else {
+    /*var swiper =*/ new Swiper(".mySwiper", {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    });
+  }
+};
+
+const widthSize = window.matchMedia("(max-width: 780px)");
+//Call listener function at run time
+myJsmedia(widthSize);
+//Attach listener function at run changes
+widthSize.addEventListener("change", myJsmedia);
+
+
+// ----- Swiper JS ------
+/*var swiper =*/ new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+//scroll to top button
+
+const heroSection = document.querySelector(".section-hero");
+const footerElem = document.querySelector(".section-footer");
+
+const scrollElement = document.createElement("div");
+scrollElement.classList.add("scrollTop-style");
+
+scrollElement.innerHTML = `<ion-icon name="arrow-up-outline" class = "scroll-top"><
+/ion-icon>`;
+
+footerElem.after(scrollElement);
+
+const scrollTop = () => {
+  heroSection.scrollIntoView({ behavior: "smooth" });
+};
+
+scrollElement.addEventListener("click", scrollTop);
+
+const workSection = document.querySelector(".section-work-data");
+
+const workSectionObserve = (entries) => {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
+    console.log(entries);
+
+
+    const counterNum = document.querySelectorAll(".counter-numbers");
+    // console.log(counterNum);
+    const speed = 100;
+
+    counterNum.forEach((curNumber) => {
+        const updateNumber = () => {
+            const targetNumber = parseInt(curNumber.dataset.number);
+            // console.log(targetNumber);
+            const initialNumber = parseInt(curNumber.innerText);
+            // console.log(initialNumber);
+            const incrementNumber = Math.trunc(targetNumber / speed);
+            // i am adding the value to the main number
+            // console.log(incrementNumber);
+
+            if (initialNumber < targetNumber) {
+                curNumber.innerText = `${initialNumber + incrementNumber}+`;
+                setTimeout(updateNumber, 10);
+            } else {
+                curNumber.innerText = `${targetNumber}+`;
+            }
+
+        };
+        updateNumber();
+    });
+};
+
+const workSecObserver = new IntersectionObserver(workSectionObserve, {
+    root: null,
+    threshold: 0,
+});
+
+workSecObserver.observe(workSection);
